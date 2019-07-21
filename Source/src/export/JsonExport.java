8938633c829c179c15887a871759c389d1fc33df
Eran <iamzealotwang@126.com>
2019-07-22 01:48:09 +0800

    no message

--- Source/src/export/JsonExport.java
+++ Source/src/export/JsonExport.java
@@ -1,6 +1,7 @@
 package export;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import data.EntityInfo;
 import utils.FileUtils;
 
@@ -9,7 +10,7 @@ import java.io.File;
 public class JsonExport {
 
     public static void Export(EntityInfo _entityInfo, String _exportPath) {
-        Gson gson = new Gson();
+        Gson gson = new GsonBuilder().disableHtmlEscaping().create();
         String json = gson.toJson(_entityInfo.entityMap);
         FileUtils.writeStringToFile(json, _exportPath + File.separator + _entityInfo.fileName + ".json");
         System.out.println("Save file to : " + _exportPath + File.separator + _entityInfo.fileName + ".json" + "\n");
